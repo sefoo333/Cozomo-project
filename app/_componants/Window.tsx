519ca72cda8b85b9b25872cb1d3e7f534715b4b5
Seifeldeen Ali <111438242+sefoo333@users.noreply.github.com>
2025-02-14 14:54:40 +0200

    Update Window.tsx

--- app/_componants/Window.tsx
+++ app/_componants/Window.tsx
@@ -11,28 +11,28 @@ function Window(props: any) {
   return (
     <>
     <div className="Window fixed w-full h-full left-0 top-0" style={{backgroundColor:"rgba(0, 0, 0, 0.397)"}}></div>
-    <div className="parent_Window w-[80%] z-20 h-[80%] fixed left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] bg-white rounded-[30px]">
-        <div className="conainter h-full flex flex-col">
+    <div className="parent_Window w-[80%] z-20 h-[80%] max-xl:w-[95%] max-lg:h-[95%] fixed left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] bg-white rounded-[30px] ">
+        <div className="conainter h-full flex flex-col overflow-y-scroll overflow-x-hidden scroll-m-0 scroll-p-0">
             <div className="exit px-[30px] pt-[20px]" onClick={() => {
                 setClick(false)
             }}>
                 <MdCancel className='text-[40px] text-right float-right  cursor-pointer text-[#859F3D]' />
             </div>
-            <div className="informations flex justify-between px-[50px] h-full gap-[60px] w-full items-center">
+            <div className="informations max-lg:flex-col flex justify-between px-[50px] h-full gap-[60px] max-lg:gap-0 w-full items-center">
                 <div className="image w-[50%] h-[90%] bg-slate-800 rounded-[30px] flex items-center overflow-hidden">
 <Image src={props.data.image} className='w-full object-contain' alt='' width={300} height={500} />
                 </div>
                 <div className="texts w-full h-[90%]">
                    <div className="head mb-[40px]">
-                   <h1 className='font-semibold text-[65px]'>{props.data.Title}</h1>
+                   <h1 className='font-semibold text-[65px] max-xl:text-[45px]'>{props.data.Title}</h1>
                    <span className='text-[20px] font-medium text-[#585858]'>{props.data.tag}</span>
                    </div>
-                    <p className='w-[650px] leading-[1.8]'>
+                    <p className='w-[650px] leading-[1.8] max-xl:w-fit'>
                     <strong>{props.data.Title} Project</strong> {props.data.description}
                         </p>
-                    <ul className='mt-[40px] text-[18px] flex flex-col gap-[10px]'>
+                    <ul className='mt-[40px] text-[18px] flex flex-col gap-[10px] py-[20px]'>
 {props.data.features.map((e:any) => (
-                            <li key={e} className='flex gap-[10px] items-center'><IoCheckmarkCircle className='text-[#859F3D] text-[25px]' /> {e}</li>
+                            <li key={e} className='flex gap-[10px] items-center text-nowrap'><IoCheckmarkCircle className='text-[#859F3D] text-[25px]' /> {e}</li>
 ))}                        
                       
                     </ul>
